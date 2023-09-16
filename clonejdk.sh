2795749315d7c143d53e14ae6180286055c47462
aaaapai <89151134+aaaapai@users.noreply.github.com>
2023-09-16 18:56:24 +0800

    Update clonejdk.sh

--- clonejdk.sh
+++ clonejdk.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
 
-git clone -b master --depth 1 https://github.com/dragonwell-project/dragonwell17 openjdk
+git clone -b dragonwell-standard-17.0.8.0.8+7_jdk-17.0.8-ga --depth 1 https://github.com/dragonwell-project/dragonwell17 openjdk
